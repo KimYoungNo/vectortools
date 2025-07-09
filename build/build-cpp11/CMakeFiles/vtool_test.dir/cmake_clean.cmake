58058ef65f50b1b486ca17a0feaecf0c075cc748
kimyoungno <kimyoungno@unist.ac.kr>
2025-07-09 06:03:41 +0000

    build plan for gcc is added

---
+++ build/build-cpp11/CMakeFiles/vtool_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vtool_test.dir/root/vectortools/src/vectortools_test.cpp.o"
+  "CMakeFiles/vtool_test.dir/root/vectortools/src/vectortools_test.cpp.o.d"
+  "vtool_test"
+  "vtool_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vtool_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
